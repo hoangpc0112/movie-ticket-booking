f150ab41ac081d29c1240050861f69f8518dd18d
sinhviennam8 <phamchinhhoang@gmail.com>
2024-10-09 18:02:09 +0700

    Thêm class xử lý exception.

---
+++ src/main/java/com/example/theater/controllers/ExceptionController.java
@@ -0,0 +1,13 @@
+package com.example.theater.controllers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        return "redirect:/";
+    }
+}
