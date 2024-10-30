72e9a744f65f47e8b850414f8cee02771674155f
sinhviennam8 <phamchinhhoang@gmail.com>
2024-10-30 19:04:36 +0700

    Sửa câu truy vấn tìm kiếm.

--- src/main/java/com/example/theater/repositories/MovieRepository.java
+++ src/main/java/com/example/theater/repositories/MovieRepository.java
@@ -12,14 +12,13 @@ public interface MovieRepository extends JpaRepository <Movie, Long> {
     List <Movie> getAllMoviesByNowShowing (boolean nowShowing);
 
     @Query (value = "SELECT * FROM movie " +
-            "WHERE (lower(title) REGEXP CONCAT('\\\\b', lower(:keyword), '[\\\\b\\\\s:,.\\\\;]*') OR " +
-            "lower(director) REGEXP CONCAT('\\\\b', lower(:keyword), '[\\\\b\\\\s:,.\\\\;]*') OR " +
-            "lower(genre) REGEXP CONCAT('\\\\b', lower(:keyword), '[\\\\b\\\\s:,.\\\\;]*') OR " +
-            "lower(actors) REGEXP CONCAT('\\\\b', lower(:keyword), '[\\\\b\\\\s:,.\\\\;]*') OR " +
-            "lower(language) REGEXP CONCAT('\\\\b', lower(:keyword), '[\\\\b\\\\s:,.\\\\;]*')) " +
+            "WHERE (lower(title) REGEXP CONCAT('[\\\\s:,.\\\\;]*', lower(:keyword), '[\\\\s:,.\\\\;]*') OR " +
+            "lower(director) REGEXP CONCAT('[\\\\s:,.\\\\;]*', lower(:keyword), '[\\\\s:,.\\\\;]*') OR " +
+            "lower(genre) REGEXP CONCAT('[\\\\s:,.\\\\;]*', lower(:keyword), '[\\\\s:,.\\\\;]*') OR " +
+            "lower(actors) REGEXP CONCAT('[\\\\s:,.\\\\;]*', lower(:keyword), '[\\\\s:,.\\\\;]*') OR " +
+            "lower(language) REGEXP CONCAT('[\\\\s:,.\\\\;]*', lower(:keyword), '[\\\\s:,.\\\\;]*')) " +
             "AND now_showing = :nowShowing", nativeQuery = true)
     List <Movie> getAllMoviesByKeyWordAndNowShowing (String keyword, boolean nowShowing);
-
 
     @Query ("select m from Movie m where m.genre like concat('%', :genre, '%') and m.nowShowing = :nowShowing ")
     List <Movie> getAllMoviesByGenreAAndNowShowing (String genre, boolean nowShowing);
